f5dd536bc842629aa9c8772559a63bdb1a4081e3
Denis Augusto <denisgusto@hotmail.com>
2024-02-01 14:54:14 -0300

    fix: edit message dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    {{ __("Bem-vindo ao painel de controle da Miner Tecnologia! 🚀") }}
                 </div>
             </div>
         </div>

54050707ce0068d62c28c47bf89c522b413b14f6
Denis Augusto <denisgusto@hotmail.com>
2024-02-01 14:52:46 -0300

    feat:create admins and users with seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@minertecnologia.com',
+            'password' => bcrypt('12345678'),
+            'is_admin' => true,
+        ]);
+
+        User::create([
+            'name' => 'Usuário padrão 1',
+            'email' => 'user1@minertecnologia.com',
+            'password' => bcrypt('12345678'),
+            'is_admin' => false,
+        ]);
+
+        User::create([
+            'name' => 'Usuário padrão 1',
+            'email' => 'user2@minertecnologia.com',
+            'password' => bcrypt('12345678'),
+            'is_admin' => false,
+        ]);
+    }
+}
